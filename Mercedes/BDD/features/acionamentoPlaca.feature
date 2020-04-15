1c715d24cbd76425fe7b110cf3d68d6b26630792
Yan Batista de Moraes <yan.moraes@ltm.digital>
2020-04-15 17:32:33 -0300

    commit Acione a Placa Premiada

---
+++ Mercedes/BDD/features/acionamentoPlaca.feature
@@ -0,0 +1,9 @@
+# language:pt
+Funcionalidade: Acionar placa premiada
+Cenario: Acionar placa premiada e fechar modal
+Dado Esteja na aba promocao
+Quando Tenha o botao acione aqui
+E Clico no acione aqui
+E E ganho premio
+E clico no X para sair
+Entao Acionei a placa premiada
---
+++ Mercedes/BDD/features/steps/acionamentoPlaca.py
@@ -0,0 +1,42 @@
+from behave import given, when, then
+import time
+
+
+@given(u'Esteja na aba promocao')
+def step_impl(context):
+    context.web.get(context.base_url + "promocoes/juntosNaEstrada/promocao")
+    time.sleep(4)
+
+
+@when(u'Tenha o botao acione aqui')
+def step_impl(context):
+    assert context.web.find_element_by_class(
+        "btn btn-premio-instantaneo ng-scope").click()
+
+
+time.sleep(4)
+
+
+@when(u'Clico no acione aqui')
+def step_impl(context):
+    context.element = context.web.find_element_by_class(
+        "btn btn-premio-instantaneo ng-scope")
+    context.element.click()
+
+
+time.sleep(4)
+
+
+@when(u'E ganho premio')
+def step_impl(context):
+    pass  # estradaPremioPremiado == 'nOK's
+
+
+@when(u'clico no X para sair')
+def step_impl(context):
+    pass  # raise NotImplementedError(u'STEP: When clico no X para sair')
+
+
+@then(u'Acionei a placa premiada')
+def step_impl(context):
+    pass  # raise NotImplementedError(u'STEP: Then Acionei a placa premiada')
